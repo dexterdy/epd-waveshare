1ebcbe8a3c56531ea30e9065830886fa3a2c80d8
Dexterdy <j.p.hagedoorn@proton.me>
2026-03-21 14:36:38 +0100

    fix: make sure x and width are byte-aligned

--- src/epd7in5_v2/mod.rs
+++ src/epd7in5_v2/mod.rs
@@ -150,20 +150,22 @@ where
             return Ok(());
         }
 
+        if x % 8 != 0 || width % 8 != 0 {
+            panic!("x and width should be byte-aligned (multiples of 8).")
+        }
+
         let expected_size = buffer_len(width as usize, height as usize);
         let actual_size = buffer.len();
         if actual_size != expected_size {
             panic!("Buffer is incorrect size. Expected: {expected_size}. Actual: {actual_size}.")
         }
 
-        let x_aligned = x & !0b111; // force to 8-bit-boundary
         let x_end = x + width - 1;
-        let x_end_aligned = x_end | 0b111; // exclusive end boundary, ending with 3 1's (following spec)
 
-        let hrst_upper = (x_aligned >> 8) as u8;
-        let hrst_lower = x_aligned as u8;
-        let hred_upper = (x_end_aligned >> 8) as u8;
-        let hred_lower = x_end_aligned as u8;
+        let hrst_upper = (x >> 8) as u8;
+        let hrst_lower = x as u8;
+        let hred_upper = (x_end >> 8) as u8;
+        let hred_lower = x_end as u8;
 
         let y_end = y + height - 1;
 
